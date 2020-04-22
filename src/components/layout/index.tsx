63f2ac54506512ec40614bb1aef0dcc10788e663
dlford <dan-github2@dlford.co>
2020-04-22 12:49:23 -0700

    code review improvements

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,18 +10,12 @@ export interface LayoutProps {
   location: Location
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, location }) => {
-  const shouldShowBigHeader: boolean = (() => {
-    return location.pathname === '/'
-  })()
-
-  return (
-    <div className='page-wrapper'>
-      <Header {...{ shouldShowBigHeader }} />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  )
-}
+const Layout = ({ children, location }: LayoutProps) => (
+  <div className='page-wrapper'>
+    <Header shouldShowBigHeader={location.pathname === '/'} />
+    <main>{children}</main>
+    <Footer />
+  </div>
+)
 
 export default Layout
